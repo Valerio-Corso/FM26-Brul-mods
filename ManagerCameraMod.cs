4da63bc727886e7dffb9adcc35ef6d762d1a4d7b
Valerio Corso <corso.valerio@gmail.com>
2025-10-31 20:33:15 +0100

    first mod

---
+++ ManagerCameraMod.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using Il2CppInterop.Runtime.Injection;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+
+namespace ManagerCameraMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class ManagerCameraMod : BasePlugin
+{
+    private new static ManualLogSource _log;
+
+    public override void Load()
+    {
+        _log = Log;
+        _log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        var go = new GameObject("ManagerCameraController");
+        Object.DontDestroyOnLoad(go);
+        
+        ClassInjector.RegisterTypeInIl2Cpp<ManagerCameraController>();
+        var component = go.AddComponent<ManagerCameraController>();
+        component.Init(_log);
+    }
+}
