5e99a3c13c82afd48bef873906aa31230eaf61d2
Valerio Corso <corso.valerio@gmail.com>
2025-11-04 22:22:23 +0100

    update .gitignore tracked files

--- CameraMod/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ManagerCameraMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "brululul.managerfreecamera";
-        public const string PLUGIN_NAME = "Manager free amera mod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
