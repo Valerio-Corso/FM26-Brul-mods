ae497695ca3de49fcb74867163c067a7b8756164
Valerio Corso <corso.valerio@gmail.com>
2025-11-05 21:33:56 +0100

    cleanup /bin

--- CameraMod/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ManagerCameraMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "brululul.managercamera";
-        public const string PLUGIN_NAME = "Manager Camera mod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- FasterMatchStartup/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace FasterMatchStartup
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "brululul.fastermatchstartup";
-        public const string PLUGIN_NAME = "FasterMatchStartup";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
